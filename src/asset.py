4fd8d5658d5ccf8715f14486bf8031c0f83f0731
mhudry <birdymaya@gmail.com>
2024-12-24 12:14:57 +0100

    commentaires asset

--- src/asset.py
+++ src/asset.py
@@ -8,6 +8,14 @@ class FinancialAsset:
         self.rdments = None
 
     def load_data(self, path, import_func):
+        """
+        Charge les données de l'actif financier à partir d'un fichier Excel ou en utilisant une fonction d'importation si le fichier excel n'est pas déjà crée.
+
+        Args:
+            path (str): Chemin vers le fichier Excel contenant les données.
+            import_func (function): Fonction pour importer les données si le fichier n'existe pas.
+        """
+
         if os.path.exists(path):
             self.data = pd.read_excel(path)
         else:
@@ -15,11 +23,26 @@ class FinancialAsset:
             self.data.to_excel(path, index=False)
     
     def compute_daily_returns(self, column_name):
+        """
+        Calcule les rendements quotidiens basés sur une colonne spécifique des données.
+
+        Args:
+            column_name (str): Nom de la colonne à partir de laquelle calculer les rendements (par exemple, 'Price' ou 'VL').
+        """
         rdment = self.data.copy()
         rdment[f'{self.name}'] = rdment[column_name].pct_change() * 100
         self.rdments = rdment[['Date', f'{self.name}']]
 
-    def compute_cumul_returns(self, returns):
+    def compute_cumul_returns(self, returns: pd.DataFrame)-> pd.DataFrame:
+        """
+        Calcule les rendements cumulés à partir des rendements quotidiens.
+        
+        Args:
+            returns (pd.DataFrame): DataFrame contenant les rendements quotidiens avec une colonne nommée après l'actif.
+        
+        Returns:
+            pd.DataFrame: DataFrame avec les rendements cumulés.
+        """
         cumul_returns = returns.copy()
         cumul_returns['Cumul Returns'] = ((cumul_returns[f'{self.name}'] / 100 + 1).cumprod() - 1) * 100
         return cumul_returns[['Date', 'Cumul Returns']]
