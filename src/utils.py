53e1f5664c8ff4f4cfa55eb27e31847e994c4be7
Alicia Verjus <alicia.verjus@gmail.com>
2024-12-24 12:17:48 +0100

    commentaires utils

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,21 @@
 import os
 import pandas as pd
-import warnings
+import warnings # Module pour gérer les avertissements
 
-from src.datafile import DataFile
+from src.datafile import DataFile # Importation de la classe DataFile depuis le module src.datafile
 
 warnings.filterwarnings('ignore')
 
 # Details fichiers par fonds analysés ------------------------------------------------------------
 def fund_loading_details():
-    
+    """
+        Définit les détails des fichiers pour les fonds analysés.
+
+        Retourne:
+            tuple: Contient trois dictionnaires (aqr_dict, jpm_dict, schroder_dict) avec les
+                   informations nécessaires pour charger les données de chaque fonds.
+        """
+
     aqr_dict = {
     "id":"US00203H4956",
     "filepath":"data/funds",
@@ -45,6 +52,15 @@ def fund_loading_details():
     return aqr_dict, jpm_dict, schroder_dict
 #---------------------------------------------------------------------------------------------------------
 def load_rfr(name):
+    """
+        Charge les données du taux sans risque (Risk-Free Rate).
+
+        Args:
+            name (str): Nom du taux sans risque à charger (par exemple, 'RF').
+
+        Retourne:
+            pd.DataFrame: DataFrame contenant les données du taux sans risque.
+        """
     if os.path.exists(rf_path:= f'data/loaded/factors/{name}.xlsx'):
         rfr = pd.read_excel(rf_path)
     else :
@@ -63,6 +79,15 @@ def load_rfr(name):
     return rfr
 #--------------------------------------------------------------------------------------------------------
 def find_unique_end_date(dataframes):
+    """
+    Trouve la date de fin unique la plus récente parmi plusieurs DataFrames.
+
+    Args:
+        dataframes (list of pd.DataFrame): Liste des DataFrames à examiner.
+
+    Retourne:
+        str: Date de fin unique minimale parmi toutes les dates de fin des DataFrames.
+    """
     end_dates = []
     for df in dataframes:
         end_dates.append(df.iloc[-1,0])
@@ -70,5 +95,16 @@ def find_unique_end_date(dataframes):
     return end_date
 #--------------------------------------------------------------------------------------------------------
 def filter(data, start_date, end_date):
+    """
+        Filtre les données entre une date de début et une date de fin spécifiées.
+
+        Args:
+            data (pd.DataFrame): DataFrame à filtrer contenant une colonne 'Date'.
+            start_date (str): Date de début pour le filtrage (format 'YYYY-MM-DD' ou similaire).
+            end_date (str): Date de fin pour le filtrage (format 'YYYY-MM-DD' ou similaire).
+
+        Retourne:
+            pd.DataFrame: DataFrame filtré contenant uniquement les lignes entre start_date et end_date inclus.
+        """
     return data[(data['Date'] >= start_date) & (data['Date'] <= end_date)]
 #--------------------------------------------------------------------------------------------------------
